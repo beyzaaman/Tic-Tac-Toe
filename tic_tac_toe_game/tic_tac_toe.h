7c7fcfea3099635c8545fcf0a83ba276fcc75436
Beyza <beyza.amann@gmail.com>
2019-07-18 17:31:30 +0300

    Header file name is changed, new line is added to end of the file, member variables are located  after the member functions.

--- tic_tac_toe_game/tic_tac_toe.h
+++ tic_tac_toe_game/tic_tac_toe.h
@@ -17,18 +17,18 @@ public:
 	void smart_computer_play();
 
 private:
+	void delete_pair(int row_,int column_);
+	void make_pair();
+	void display_game();
+	int gameplay(int row,int column);
+	close_t winnable_cases();
+
+private:
 	std::vector<std::vector<char>> game_;
 	std::vector<std::pair<int, int>> location_vector_;
 	int							   row_;
 	int							   column_;
 	bool						   player_;
 
-
-private:
-	void delete_pair(int row_,int column_);
-	void make_pair();
-	void display_game();
-	int gameplay(int row,int column);
-	close_t winnable_cases();
 	
 };
